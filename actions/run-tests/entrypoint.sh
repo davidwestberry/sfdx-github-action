f214d227d82588b612d8ee1b6d6965a403630563
David Westberry <jdavidwestberry@gmail.com>
2020-08-26 11:32:31 -0500

    Update entrypoint.sh

--- actions/run-tests/entrypoint.sh
+++ actions/run-tests/entrypoint.sh
@@ -15,13 +15,17 @@ sfdx force:config:set --global instanceUrl=${INSTANCE_URL}
 
 RESULT=$(sfdx force:apex:test:run -c --testlevel RunLocalTests --json -r json -w 15 -u ${ACCESS_TOKEN})
 
-OUTCOME=$(echo ${RESULT} | jq -r .result.summary.outcome)
+OUTCOME=$(echo ${RESULT} | jq -r '.result.summary.outcome')
 
 if [[ ${OUTCOME} -eq "Failed" ]]; then
     echo "Test failures were reported"
-    echo "${RESULT}"
+    #echo "${RESULT}"
     echo "${RESULT}" | jq -r '.result.summary'
     FAILURES=$(echo "${RESULT}" | jq -r '[.result.tests[] | select(.Outcome == "CompileFail" or .Outcome == "Fail") | {"test": .FullName, "stackTrace": .StackTrace, "message": .Message}]')
     echo ${FAILURES}
     exit 1
+else
+    echo "Tests Passed"
+    echo "${RESULT}" | jq -r '.result.summary'
+    exit 0
 fi
